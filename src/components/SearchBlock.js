d8fa6e48cd3cd16a887ccfc24f3b4f631e93f161
Kanstantsin Sakavets <kanstantsin.sakavets@gmail.com>
2020-06-13 14:57:04 +0300

    start refactor project

---
+++ src/components/SearchBlock.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+
+const SearchBlock = () => {
+    const searchText = 'Search';
+    return <input placeholder = {searchText}
+    />;
+}
+
+export default SearchBlock;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBlock from './components/SearchBlock.js'
 
-const el = <h1>Реакт элемент</h1>;
 
-ReactDOM.render(el,document.getElementById('root'));
+// const h1 = React.createElement(
+//   'h1',
+//   {className: 'greeting'},
+//   'Привет, мир!'
+// );
+const isLogged = true;
 
+const LoginBlock = <span>sign in</span>;
+const welcome = <span>Hello</span>;
+
+
+const Header = ()=>{
+  const st = {
+    color:'red',
+    fontSize: '60px'
+  }
+  return <h1 style={st}>Todo List</h1>;
+}
+
+const TodoList = ()=>{
+  const items=['Learn HTML', 'Learn CSS', 'Learn React']
+  return(
+    <ul>
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
+      <li>{items[2]}</li>
+    </ul>
+  )
+}
+
+const App = ()=>{
+  return (
+    <div>
+      {isLogged ? welcome:LoginBlock }
+      {/* {h1} */}
+    <Header />
+    <SearchBlock />
+    <TodoList />
+    </div>
+  );
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'));
